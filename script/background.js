64b68c8ebd2f0cff5d003a4127444dfd70e1df01
fanstein <fanfly1607@163.com>
2018-06-27 22:11:12 +0800

    get cookie

--- script/background.js
+++ script/background.js
@@ -1,30 +1,36 @@
 chrome.contextMenus.create({
-    id:"menu",
-    title: "Cookie",
-    contexts:["all"],
-    // onclick: function(){alert('您点击了右键菜单！');}
-}),
-chrome.contextMenus.create({
-    id:"clear",
-    parentId:"menu",
-    title:"Clear Cookie",
-    onclick:function(){
-        alert("chear cookie")
-    },
-    contexts: ["all"]
-}),
-chrome.contextMenus.create({
-    id: "separator3",
-    parentId: "menu",
-    type: "separator",
-    contexts: ["all"]
-}),
-chrome.contextMenus.create({
-    id: "view",
-    parentId: "menu",
-    title: "view Cookie",
-    onclick: function(){
-        alert("view cookie")
-    },
-    contexts: ["all"]
-});
+        id: "menu",
+        title: "Cookie",
+        contexts: ["all"],
+        // onclick: function(){alert('您点击了右键菜单！');}
+    }),
+    chrome.contextMenus.create({
+        id: "view",
+        parentId: "menu",
+        title: "View Cookie",
+        onclick: function () {
+            var cook='';
+            chrome.cookies.getAll({},function(cookie) {
+                cookie.forEach(function(c){ 
+                    cook = cook + c.name + '=' + c.value + ';'
+                });
+            alert(cook);
+            })
+        },
+        contexts: ["all"]
+    }),
+    chrome.contextMenus.create({
+        id: "separator3",
+        parentId: "menu",
+        type: "separator",
+        contexts: ["all"]
+    }),
+    chrome.contextMenus.create({
+        id: "clear",
+        parentId: "menu",
+        title: "Clear Cookie",
+        onclick: function() {
+            alert("clear cookie")
+        },
+        contexts: ["all"]
+    });
