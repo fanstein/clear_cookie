6104a99d0dde96bab9fbc30fbb4074f91903e9c9
fanp <fanfly1607@163.com>
2018-06-28 14:19:41 +0800

    'can_view'

--- script/background.js
+++ script/background.js
@@ -9,13 +9,32 @@ chrome.contextMenus.create({
         parentId: "menu",
         title: "View Cookie",
         onclick: function () {
+            // var _url ='';
+            // chrome.tabs.getSelected(null, function (tab) {
+            //     // alert(tab.url);
+            //     _url = tab.url;
+            // });
+            // _host= _url.split('/')[2];
+            // _domain = _host.substring(_host.indexof('.'), _host.length);
+            // var cook='';
+            // chrome.cookies.getAll({url:'https://crxdoc-zh.appspot.com/extensions/cookies'},function(cookie) {
+            //     cookie.forEach(function(c){ 
+            //         cook = cook + c.name + '=' + c.value + ';'
+            //     });
+            // alert(cook);
+            // })
+
             var cook='';
-            chrome.cookies.getAll({},function(cookie) {
-                cookie.forEach(function(c){ 
-                    cook = cook + c.name + '=' + c.value + ';'
-                });
-            alert(cook);
-            })
+            chrome.tabs.getSelected(null, function (tab) {
+                chrome.cookies.getAll({url:tab.url},function(cookie) {
+                    cookie.forEach(function(c){ 
+                        cook = cook + c.name + '=' + c.value + ';'
+                    });
+                alert(cook);
+                })
+                // alert(tab.url);
+                _url = tab.url;
+            });
         },
         contexts: ["all"]
     }),
@@ -30,7 +49,10 @@ chrome.contextMenus.create({
         parentId: "menu",
         title: "Clear Cookie",
         onclick: function() {
-            alert("clear cookie")
+            chrome.tabs.getSelected(null, function (tab) {
+                alert(tab.url);
+            });
+            // alert("clear cookie")
         },
         contexts: ["all"]
     });
